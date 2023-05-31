4f012ca8746410d77c4b4173e0a9e37213c5761f
Karina Khoninova <k.honinova@yandex.ru>
2023-06-01 00:59:47 +0300

    Add project files.

---
+++ Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingEnums
+{
+    class Card
+    {
+        public Suits suit { get; private set; }
+        public Values value { get; private set; };
+
+        public string Name { get { return $"{value} of {suit}"; } }
+
+        public Card(Values value, Suits suit)
+        {
+            this.suit = suit;
+            this.value = value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UnderstandingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomSuit = random.Next(4);
+            int randomValue = random.Next(1, 14);
+
+            Card myCard = new Card((Values)randomValue, (Suits)randomSuit);
+            Console.WriteLine(myCard.Name);
+        }
+    }
+}
